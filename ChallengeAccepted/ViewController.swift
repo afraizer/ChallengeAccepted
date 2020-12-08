e7098daa28f301825b697206488bf62e827e4847
Clare Casey <ccasey5@zagmail.gonzaga.edu>
2020-12-08 10:40:49 -0800

    New Project!

--- ChallengeAccepted/ViewController.swift
+++ ChallengeAccepted/ViewController.swift
@@ -6,11 +6,33 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class ViewController: UIViewController {
+    var score = 0
+    var password = "Password"
+    var email = "123@gmail.com"
+    
+    /*
+    // put this in an ib action
+    var ref: DocumentReference? = nil
+    ref = db.collection("users").addDocument(data: [
+        "first": "Ada",
+        "last": "Lovelace",
+        "born": 1815
+    ]) { err in
+        if let err = err {
+            print("Error adding document: \(err)")
+        } else {
+            print("Document added with ID: \(ref!.documentID)")
+        }
+    }
+    */
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        FirebaseApp.configure()
         // Do any additional setup after loading the view.
     }
 
